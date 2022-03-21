a5ac8ae88b384e58dd29eb20374e50d9a1ece6f6
V-i-m-a-l <101229988+V-i-m-a-l@users.noreply.github.com>
2022-03-21 06:59:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let count = 0
+
+// document.getElementById("___").innerHTML = 5
+// THE 1ST PART (BEFOR INNER...) HELPS US TO ACCESS THE ELEMENT
+// THE 2ND PART ALLOWS US TO MODIFY THE ELEMENT
+
+counter = document.getElementById("counting")
+// Accessed the element and assigned it to counter var
+
+
+function increment(){
+
+    count += 1
+//increment the count var by 1
+    counter.innerHTML = count
+//assign the new count (incremented) var to the element's innerHTML (which is 0)
+}
